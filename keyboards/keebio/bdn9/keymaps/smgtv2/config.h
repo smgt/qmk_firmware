37b9d00b555ed267d8b76ee3b9dc2feaa44e5bc4
Simon Gate <simon@kampgate.se>
2021-11-27 20:51:16 +0100

    Add a bdn9 smgt test version

---
+++ keyboards/keebio/bdn9/keymaps/smgtv2/config.h
@@ -0,0 +1,10 @@
+#define USB_MAX_POWER_CONSUMPTION 100
+
+#undef VENDOR_ID
+#undef PRODUCT_ID
+#undef DEVICE_VER
+#undef PRODUCT
+#define VENDOR_ID       0xCB10
+#define PRODUCT_ID      0x3133
+#define DEVICE_VER      0x0300
+#define PRODUCT         BDN9 Rev. 1 smgt
