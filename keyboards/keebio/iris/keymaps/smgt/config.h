0c68dc3e2edae9ccdad4023935fb0bc0d5d8625f
Simon Gate <simon@kampgate.se>
2023-10-11 15:44:10 +0200

    [smgt] Update iris smgt layout for latest QMK

--- keyboards/keebio/iris/keymaps/smgt/config.h
+++ keyboards/keebio/iris/keymaps/smgt/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@keeb.io>
+Copyright 2017 Danny Nguyen <danny@hexwire.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,7 +15,26 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
 
+/* Use I2C or Serial, not both */
+
+#define USE_SERIAL
 // #define USE_I2C
-#define EE_HANDS
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+#define USB_MAX_POWER_CONSUMPTION 100
+
+// #undef RGBLED_NUM
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 12
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+
+#endif
